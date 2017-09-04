4aaf100fefdc77280f1e856cd0e8456adaa5285f
Gilberto Vargas <tachoguitar@gmail.com>
2017-09-03 23:45:55 -0500

    Add endpoint for registering users to sites

---
+++ routes/auth.rb
@@ -0,0 +1,32 @@
+require 'ant'
+require_relative '../controllers/auth'
+
+module Gate
+  module Routes
+    class Auth < Grape::API
+      include Exceptions
+
+      def self.build_auth(params)
+        pass = params[:pass]
+        params[:pass] = '*' * 8
+        site = Controllers::Sites.new(params[:site])
+        raise(SiteNotFound, params[:site]) unless site.exists?
+        user = Controllers::User.new(params[:user])
+        raise(UserNotFound, params[:user]) unless user.exists?
+        Controllers::Auth.new(site, user, pass)
+      end
+
+      namespace :sites do
+        route_param :site do
+          post :register do
+            process_request do
+              auth = Auth.build_auth(params)
+              auth.register
+              auth.to_h
+            end
+          end
+        end
+      end
+    end
+  end
+end
