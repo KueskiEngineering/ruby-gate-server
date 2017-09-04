227f3daa9cbcfe0cbfaa89b0ed747175b7e6c39d
Gilberto Vargas <tachoguitar@gmail.com>
2017-09-03 23:33:28 -0500

    Implement site table

---
+++ model/migrations/001_add_sites_table.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+Sequel.migration do
+  up do
+    $SEQUEL_VERBOSE = true
+    create_table(:site) do
+      primary_key :site_id, auto_increment: true
+      String   :name, null: false, size: 30, unique: true
+      DateTime :created_at, null: false
+      DateTime :updated_at, null: false
+      Bool     :enabled, null: false, default: true
+    end
+  end
+end
