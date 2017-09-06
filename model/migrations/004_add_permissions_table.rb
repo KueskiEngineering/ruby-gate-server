454e4a21a6fc13ea8f36542c7a982c33abecda14
Gilberto Vargas <tachoguitar@gmail.com>
2017-09-06 00:03:14 -0500

    Add table for storing permissions

---
+++ model/migrations/004_add_permissions_table.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+Sequel.migration do
+  up do
+    create_table(:permission) do
+      primary_key :permission_id
+      foreign_key :site_id, :site
+      unique %i[site_id name]
+      String   :name, null: false, size: 30
+      DateTime :created_at, null: false
+      DateTime :updated_at, null: false
+      Bool     :enabled, null: false, default: true
+    end
+  end
+end
