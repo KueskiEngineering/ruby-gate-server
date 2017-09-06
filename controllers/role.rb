178ab285859954e7a2184ceb8002d8ad087b47d7
Gilberto Vargas <tachoguitar@gmail.com>
2017-09-06 00:04:30 -0500

    Add role controller

---
+++ controllers/role.rb
@@ -0,0 +1,43 @@
+# frozen_string_literal: true
+
+require_relative '../lib/services'
+require_relative '../lib/exception'
+require_relative 'name_list_controller'
+
+module Gate
+  module Controllers
+    ##
+    # this class handles user
+    class Role < NameListController
+      include Exceptions
+      def initialize(name, site)
+        @site = site
+        super(name)
+      end
+
+      def id_field
+        :role_id
+      end
+
+      def name_field
+        :name
+      end
+
+      def control_table
+        :role
+      end
+
+      def extra_fields
+        { site_id: @site.id }
+      end
+
+      def already_exists_exception
+        RoleAlreadyExists
+      end
+
+      def primary_key_hash
+        { name_field => name, site_id: @site.id }
+      end
+    end
+  end
+end
