cf21ecef1b9b9bca8602868e64f4711d80166c3e
Gilberto Vargas <tachoguitar@gmail.com>
2017-09-06 00:06:32 -0500

    Add controller for assigned roles

---
+++ controllers/user_role.rb
@@ -0,0 +1,28 @@
+# frozen_string_literal: true
+require_relative 'relationship_controller'
+
+module Gate
+  module Controllers
+    class UserRole < RelationshipController
+      def main_field
+        :role_id
+      end
+
+      def second_field
+        :user_id
+      end
+
+      def control_table
+        :user_role
+      end
+
+      def id_field
+        :user_role_id
+      end
+
+      def already_exists_exception
+        PermissionAlreadyAssigned
+      end
+    end
+  end
+end
