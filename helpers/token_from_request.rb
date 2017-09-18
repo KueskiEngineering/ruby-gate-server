bdc6556ddf7c887e0045bacfdd75410d1857bfc5
Gilberto Vargas <tachoguitar@gmail.com>
2017-09-17 22:02:23 -0500

    change token from request helper to accept site

--- helpers/token_from_request.rb
+++ helpers/token_from_request.rb
@@ -1,6 +1,6 @@
 module Gate
   module TokenFromRequest
-    def token_from_request
+    def token_from_request(site = nil)
       begin
         body_params = JSON.parse(request.body.read, symbolize_names: true)
         token = body_params[:token] || params[:token]
@@ -8,7 +8,7 @@ module Gate
         token = body_params[:token] || params[:token]
       end
       params[:token] = '*' * 8
-      Controllers::Token.parse(token)
+      Controllers::Token.parse(token, site || params[:site])
     end
   end
 end
